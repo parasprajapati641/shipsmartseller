cf9be78f46e5d183cfad4bb985359ce3e8bc16e0
parasprajapati641 <parasprajapati641@gmail.com>
2026-07-25 17:58:30 +0530

    Remove login and signup functionality

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
+const cors = require("cors");
 const express = require("express");
 const mongoose = require("mongoose");
-const cors = require("cors");
 require("dotenv").config();
 
-
 const app = express();
-
 
 // Middleware
 app.use(cors());
